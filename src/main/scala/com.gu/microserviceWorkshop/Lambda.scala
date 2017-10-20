7d1f0e068b671843c9601129d19bfcdf1955e31d
Frédéric Gauchet <frederic.gauchet@theguardian.com>
2017-10-20 17:55:23 +0100

    Returns a response that AWS_PROXY understands

--- src/main/scala/com.gu/microserviceWorkshop/Lambda.scala
+++ src/main/scala/com.gu/microserviceWorkshop/Lambda.scala
@@ -1,7 +1,16 @@
 package com.gu.microserviceWorkshop
 
+import io.circe.syntax._
+import java.io._
+import java.nio.charset.StandardCharsets.UTF_8
+
 object Lambda {
-  def handler(): String = {
-    "Hello world"
+  def handler(in: InputStream, out: OutputStream): Unit = {
+
+    val response = APIResponse(200,  Map("Content-Type" -> "application/json"), "hello world again")
+
+    //no spaces converts json to a string
+    out.write(response.asJson.noSpaces.getBytes(UTF_8))
+
   }
 }
