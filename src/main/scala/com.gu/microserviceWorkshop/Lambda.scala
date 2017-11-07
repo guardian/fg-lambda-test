b9488f52d437cd83438635f5d6cd9b8a7c9875df
Frédéric Gauchet <frederic.gauchet@theguardian.com>
2017-11-07 11:23:11 +0000

    Improves error handling

--- src/main/scala/com.gu/microserviceWorkshop/Lambda.scala
+++ src/main/scala/com.gu/microserviceWorkshop/Lambda.scala
@@ -10,13 +10,17 @@ object Lambda {
   def handler(in: InputStream, out: OutputStream): Unit = {
 
     val inputString = Source.fromInputStream(in).mkString("")
-    System.out.println(inputString)
     // decode[APIInput](inputString) returns Either
-    System.out.println(decode[APIRequest](inputString).left.map(_.toString))
     val body = decode[APIRequest](inputString).toOption.map(x => x.body)
-    val request = decode[APIInput](body.getOrElse("")).toOption.map(x => x.value.toString)
-
-    val response = APIResponse(200,  Map("Content-Type" -> "application/json"), request.getOrElse("Invalid input"))
+    val response = body match {
+      case Some(s) => {
+        val value = decode[APIInput](s).toOption.map(x => x.value)
+        APIResponse(200,  Map("Content-Type" -> "application/json"), "Value is " + value.toString)
+      }
+      case None => {
+        APIResponse(200,  Map("Content-Type" -> "application/json"), "Error decoding body")
+      }
+    }
 
     //no spaces converts json to a string
     out.write(response.asJson.noSpaces.getBytes(UTF_8))
